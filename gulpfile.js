afabc029b97c56a7ad40066b310fef729ee7b055
renanlblu <cursosprogramacoes@gmail.com>
2019-05-29 23:06:49 -0300

    Add first test

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 
-gulp.task('scss', function () {
+
+gulp.task('scss', () => {
   return gulp.src('./src/scss/*.scss')
   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
   .pipe(gulp.dest('./src/css'));
 })
 
 gulp.task('watch', function () {
-  gulp.watch('./src/scss/*.scss', gulp.series('scss'));
+  gulp.watch('./src/scss/**/*.scss', gulp.series('scss'));
 })
+
 
 gulp.task('default', () => {
   console.log("Running...")
